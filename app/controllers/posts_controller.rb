4e1c1bb8e9e831bc80f52d36fb6a72f09ef12a48
dhav211 <dhav211@gmail.com>
2024-02-17 09:49:46 -0800

    Add edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
   end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id]
+    if @post.update(allowed_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:post).permit(:title, :text, :thumbnail_url)
+  end
 end
